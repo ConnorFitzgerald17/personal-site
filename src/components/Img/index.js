8e3145e21d1697e34bc3d993c9b05518e9a6e200
ConnorFitzgerald17 <connorfitzgerald17@gmail.com>
2023-09-24 19:01:06 -0400

    new v2

--- src/components/Img/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Img = ({ file, title }) => {
-  return <img src={file} alt={title}></img>
-}
-
-export default Img
--- src/components/P/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-const P = styled.p`
-  color: #666d71;
-  font-size: 1.6em;
-  padding: 0 20px;
-`
-
-export default P
